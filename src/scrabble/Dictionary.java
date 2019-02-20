76814de24cc23847bf38d5aa333b4a229da018a2
craigparry <parryc@unm.edu>
2019-02-20 14:48:12 -0700

    made my dictionary into trienodes with hashmaps as their branches and updated the search and insert methods to traverse recursively

--- src/scrabble/Dictionary.java
+++ src/scrabble/Dictionary.java
@@ -11,7 +11,9 @@ public class Dictionary {
     */
     private List<TrieNode> dictionary;
     private List<String> hold;
+    private Map<Character,TrieNode> dictMap;
     public Dictionary(){
+        dictMap = new HashMap<>();
         dictionary = new LinkedList<>();
         hold = new LinkedList<>();
         readDictFile();
@@ -43,129 +45,183 @@ public class Dictionary {
      */
     public void populate(){
         for(String s: hold){
-            insert(s);
+           insert(s,null,0, s.length());
+            //insert(s);
         }
     }
 
-    public void insert(String word){
+//    public void insert(String word){
+//        word.toLowerCase();
+//        int length = word.length();
+//        // if the dictionary is empty make a new node
+//        if(dictionary.isEmpty()){
+//            dictionary.add(new TrieNode(word.charAt(0)));
+//        }
+//        // finds the node corresponding to the letter
+//        TrieNode temp = null;
+//        for(TrieNode t: dictionary){
+//            if(word.charAt(0) == t.getLetter()){
+//                temp = t;
+//                break;
+//            }
+//        }
+//        // creates node for new letter in the branch
+//        if(temp == null){
+//            temp = new TrieNode(word.charAt(0));
+//            dictionary.add(temp);
+//        }
+//
+//        // starting at 1 for the length of the word find the letters in the branches
+//        int i=1;
+//        do{
+//            boolean exists = false;
+//
+//            if(temp.getBranch().isEmpty()){
+//                temp.getBranch().add(new TrieNode(word.charAt(i)));
+//            }
+//
+//            for(TrieNode t: temp.getBranch()){
+//                if(word.charAt(i) == t.getLetter()){
+//                    temp = t;
+//                    exists = true;
+//                    break;
+//                }
+//
+//            }
+//            if(!exists){
+//                TrieNode hold = new TrieNode(word.charAt(i));
+//                temp.getBranch().add(hold);
+//                temp = hold;
+//            }
+//            if(i == length -1){
+//                temp.isWord();
+//            }
+//            i++;
+//
+//        }while(i<length);
+//    }
+
+    public void insert(String word, TrieNode node, int position, int length){
         word.toLowerCase();
-        int length = word.length();
-
-        if(dictionary.isEmpty()){
-            dictionary.add(new TrieNode(word.charAt(0)));
+//        int length = word.length();
+        TrieNode temp;
+        if(node == null){
+            dictMap.putIfAbsent(word.charAt(position), new TrieNode(word.charAt(position)));
+            temp = dictMap.get(word.charAt(position));
+        } else {
+            node.getMapBranch().putIfAbsent(word.charAt(position), new TrieNode(word.charAt(position)));
+            temp = node.getMapBranch().get(word.charAt(position));
         }
 
-        TrieNode temp = null;
-        for(TrieNode t: dictionary){
-            if(word.charAt(0) == t.getLetter()){
-                temp = t;
-                break;
-            }
+
+        if(position == length-1){
+            temp.setWord();
+            return;
+        } else {
+            insert(word,temp,position+1,length);
         }
 
-        if(temp == null){
-            temp = new TrieNode(word.charAt(0));
-            dictionary.add(temp);
-        }
 
-        int i=1;
-        do{
-            boolean exists = false;
 
-            if(temp.getBranch().isEmpty()){
-                temp.getBranch().add(new TrieNode(word.charAt(i)));
-            }
-
-            for(TrieNode t: temp.getBranch()){
-                if(word.charAt(i) == t.getLetter()){
-                    temp = t;
-                    exists = true;
-                    break;
-                }
-
-            }
-            if(!exists){
-                TrieNode hold = new TrieNode(word.charAt(i));
-                temp.getBranch().add(hold);
-                temp = hold;
-            }
-            if(i == length -1){
-                temp.isWord();
-            }
-            i++;
-
-        }while(i<length);
     }
-
-    /**Searches the Trie for the word passed into the function
-     * @param word
-     * @return String
-     */
-    public boolean search(String word){
+    public boolean search(String word, TrieNode node, int position, int length){
         word.toLowerCase();
-        int length = word.length();
-
-
-        List<TrieNode> hold = dictionary;
-
-        boolean found = false;
+        if(position == length) return false;
         TrieNode temp = null;
-        for(TrieNode t: dictionary){
-            if(word.charAt(0) == t.getLetter()){
-                temp = t;
+        if(node == null){
+            if(dictMap.containsKey(word.charAt(position))){
+                temp = dictMap.get(word.charAt(position));
+                return search(word, temp, position+1, length);
+            } else return false;
+
+        } else{
+            if(node.getMapBranch().containsKey(word.charAt(position))){
+                temp = node.getMapBranch().get(word.charAt(position));
             }
         }
-        if(temp == null){
-            return false;
-        }
-        int i=1;
 
-        do{
-
-           for(TrieNode t: temp.getBranch()){
-               if(word.charAt(i) == t.getLetter()){
-
-                   temp =t;
-                   break;
-               }
-           }
-           if(temp != null && i == length-1 && temp.isWord){
-               found = true;
-           }
-            i++;
-
-        }while(i<length);
-
-        return found;
+        if(temp == null) return false;
+        if(position == length-1 && temp.isWord()){
+            return true;
+        } else return search(word, temp, position+1,length);
     }
-    /**Reads in file from input
-     *
-     * @param file
-     * @return void
-     */
-    public void readFile(String file){
-        hold = new LinkedList<>();
-
-        /*read in the file and store it in the hold list*/
 
 
-    }
+//    /**Searches the Trie for the word passed into the function with Linkedlist
+//     * trie
+//     * @param word
+//     * @return String
+//     */
+//    public boolean search(String word){
+//        word.toLowerCase();
+//        int length = word.length();
+//
+//
+//        List<TrieNode> hold = dictionary;
+//
+//        boolean found = false;
+//        TrieNode temp = null;
+//        for(TrieNode t: dictionary){
+//            if(word.charAt(0) == t.getLetter()){
+//                temp = t;
+//            }
+//        }
+//        if(temp == null){
+//            return false;
+//        }
+//        int i=1;
+//
+//        do{
+//
+//           for(TrieNode t: temp.getBranch()){
+//               if(word.charAt(i) == t.getLetter()){
+//
+//                   temp =t;
+//                   break;
+//               }
+//           }
+//           if(temp != null && i == length-1 && temp.isWord){
+//               found = true;
+//           }
+//            i++;
+//
+//        }while(i<length);
+//
+//        return found;
+//    }
+
+//    /**Reads in file from input
+//     *
+//     * @param file
+//     * @return void
+//     */
+//    public void readFile(String file){
+//        hold = new LinkedList<>();
+//
+//        /*read in the file and store it in the hold list*/
+//
+//
+//    }
 
     public class TrieNode{
         private String word;
         private boolean isWord;
         private List<TrieNode> branch;
+        private Map<Character, TrieNode> mapBranch;
         private char letter;
 
         public TrieNode(char let){
             letter = let;
+            mapBranch = new HashMap<>();
             branch = new LinkedList<>();
-
+            isWord = false;
         }
+        public void setWord(){
+            isWord =true;
+        }
+        public boolean isWord(){
 
-        public void isWord(){
-            isWord = true;
-
+            return isWord;
         }
         public List<TrieNode> getBranch(){
             return branch;
@@ -174,6 +230,12 @@ public class Dictionary {
         public char getLetter(){
             return letter;
         }
+
+        public Map<Character, TrieNode> getMapBranch() {
+            return mapBranch;
+        }
+
+
     }
 
     public static void main(String[] args){
@@ -192,16 +254,28 @@ public class Dictionary {
 
 
 
-        if(test.search("there")){ System.out.println("found");}
-        if(test.search("brother")){ System.out.println("found");}
-        if(test.search("bo")){ System.out.println("found");}
+//        if(test.search("there")){ System.out.println("found");}
+//        if(test.search("brother")){ System.out.println("found");}
+//        if(test.search("bo")){ System.out.println("found");}
+//
+//        if(test.search("stess")){ System.out.println("found");}
+//        if(test.search("test")){ System.out.println("found");}
+//        if(test.search("aqui")){
+//            System.out.println("found");
+//        } else {System.out.println("Not Found");}
 
-        if(test.search("stess")){ System.out.println("found");}
-        if(test.search("test")){ System.out.println("found");}
-        if(test.search("aqui")){
+
+
+        if(test.search("there",null,0,5)){ System.out.println("found");}
+        if(test.search("brother",null,0,7)){ System.out.println("found");}
+        if(test.search("bo",null,0,2)){ System.out.println("found");}
+
+        if(test.search("stress",null,0,6)){ System.out.println("found");}
+        if(test.search("test",null,0,4)){ System.out.println("found");}
+//        System.out.println("here");
+        if(test.search("aqui",null,0,4)){
             System.out.println("found");
         } else {System.out.println("Not Found");}
-
     }
 
 }
